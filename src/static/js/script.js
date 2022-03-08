f2163ccf36191c0545a7c98f86daceb2e16af78d
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-03-08 20:08:38 +0700

    Move inline javascript to html

---
+++ src/static/js/script.js
@@ -0,0 +1,12 @@
+const tradeOption = document.getElementById('tradeOption');
+const actionBtn = document.getElementById('action');
+const checkMultiplier = document.getElementById('enableMultiplier');
+
+actionBtn.innerText = tradeOption.value==='buy' ? 'Mua' : 'Bán';
+
+tradeOption.addEventListener('change', function() {
+    actionBtn.innerText = this.value==='buy'?'Mua':'Bán';
+});
+checkMultiplier.addEventListener('change', function() {
+    document.getElementById('multiplier').disabled = !this.checked
+});
