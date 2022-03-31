188a3e69135ff38b7a1ca57a7989f2e23c40cbf6
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-03-31 16:53:39 +0700

    Index page showing null values fix

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -11,21 +11,21 @@ socket.on('receive_data', data => {
         tr += '<tr>';
         tr +=
             '<td>' + item.macp.trim() + '</td>' +
-            '<td>' + item.giaM3 + '</td>' +
-            '<td>' + item.soluongM3 + '</td>' +
-            '<td>' + item.giaM2 + '</td>' +
-            '<td>' + item.soluongM2 + '</td>' +
-            '<td>' + item.giaM1 + '</td>' +
-            '<td>' + item.soluongM1 + '</td>' +
-            '<td>' + item.giaKhop + '</td>' +
-            '<td>' + item.soluongKhop + '</td>' +
-            '<td>' + item.giaB1 + '</td>' +
-            '<td>' + item.soluongB1 + '</td>' +
-            '<td>' + item.giaB2 + '</td>' +
-            '<td>' + item.soluongB2 + '</td>' +
-            '<td>' + item.giaB3 + '</td>' +
-            '<td>' + item.soluongB3 + '</td>' +
-            '<td>' + item.tongKL + '</td>';
+            '<td>' + (item.giaM3 == null ? "" : item.giaM3) + '</td>' +
+            '<td>' + (item.soluongM3 == null ? "" : item.soluongM3) + '</td>' +
+            '<td>' + (item.giaM2 == null ? "" : item.giaM2) + '</td>' +
+            '<td>' + (item.soluongM2 == null ? "" : item.soluongM2) + '</td>' +
+            '<td>' + (item.giaM1 == null ? "" : item.giaM1) + '</td>' +
+            '<td>' + (item.soluongM1 == null ? "" : item.soluongM1) + '</td>' +
+            '<td>' + (item.giaKhop == null ? "" : item.giaKhop) + '</td>' +
+            '<td>' + (item.soluongKhop == null ? "" : item.soluongKhop) + '</td>' +
+            '<td>' + (item.giaB1 == null ? "" : item.giaB1) + '</td>' +
+            '<td>' + (item.soluongB1 == null ? "" : item.soluongB1) + '</td>' +
+            '<td>' + (item.giaB2 == null ? "" : item.giaB2) + '</td>' +
+            '<td>' + (item.soluongB2 == null ? "" : item.soluongB2) + '</td>' +
+            '<td>' + (item.giaB3 == null ? "" : item.giaB3) + '</td>' +
+            '<td>' + (item.soluongB3 == null ? "" : item.soluongB3) + '</td>' +
+            '<td>' + (item.tongKL == null ? "" : item.tongKL) + '</td>';
         tr += '</tr>'
     })
     tableBody.innerHTML = tr;
